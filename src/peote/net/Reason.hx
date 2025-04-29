b8261b33e019f2d3436c57ce441607bb7893fc31
0bk1n00b <polysemantic@gmail.com>
2025-04-29 11:55:43 +0200

    oops

--- src/peote/net/Reason.hx
+++ src/peote/net/Reason.hx
@@ -2,28 +2,30 @@ package peote.net;
 
 enum abstract Reason(Int) from Int to Int
 {
-	inline var DISCONNECT :Int = 0; // disconnected from peote-server (joint-owner/user)
-	inline var CLOSE      :Int = 1; // owner closed joint or user leaves
-	inline var KICK       :Int = 2; // user was kicked by joint-owner
-	                                    
-	inline var ID         :Int = 10; // can't enter/open joint with this id (another or none exists)
-	inline var FULL       :Int = 11; // channel is full (max of 256 users already connected) (max is 128).
-	inline var MAX        :Int = 12; // created/joined to much channels on this server (max is 128).
-                                        
-	inline var MALICIOUS  :Int = 20; // malicious input
-}
-class ReasonLift{
-	static public function toString(self:Reason){
-		return switch(self){
+	var DISCONNECT = 0; // disconnected from peote-server (channel-owner/user)
+	var CLOSE      = 1; // owner closed channel or user leaves
+	var KICK       = 2; // user was kicked by channel-owner (TODO!!!)
+
+	var ID         = 10; // can't enter/open channel with this id (server: wrong id or already used, client: no chan with this id)
+	var FULL       = 11; // channel is full (max of 256 users already connected) (max is 128)
+	var MAX        = 12; // server created or client joined to much channels on this server (max is 128)
+
+	var MALICIOUS  = 20; // malicious input
+
+	@:to public function toString():String {
+		return switch(this)
+		{
 			case DISCONNECT : "DISCONNECT"; 
 			case CLOSE      : "CLOSE"; 
-			case KICK       : "KICK"; 
-	                                    
+			case KICK       : "KICK"; // not yet implemented !!!
+
 			case ID         : "ID"; 
 			case FULL       : "FULL"; 
 			case MAX        : "MAX"; 
-                                        
+
 			case MALICIOUS  : "MALICIOUS";
+
+			default: "unknown";
 		}
 	}
 }
